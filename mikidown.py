74dad2ad0b7a41e151530d7bfda8b1cc7aa137fe
CKolumbus <ckolumbus@ac-drexler.de>
2014-04-23 21:23:22 +0200

    adjustment for python2 compatibility and reSt text
    * made mikidown lib working with python2 (no edit, only other widgets)
    * tested mikidown itself, not working (unicode issues remain)
    * needed to patch mikitree.py to support .rst extenstion (needs refactoring)

--- mikidown.py
+++ mikidown.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# http://pyqt.sourceforge.net/Docs/PyQt4/incompatible_apis.html
+import sip
+sip.setapi('QString', 2)
+sip.setapi('QVariant', 2)
+
 import sys
 import logging
 import mikidown
@@ -9,8 +14,9 @@ try:
     version_info = sys.version_info
     assert version_info > (3, 0)
 except:
-    print('ERROR: mikidown needs python >= 3.0', file=sys.stderr)
-    sys.exit(1)
+    #print('ERROR: mikidown needs python >= 3.0', file=sys.stderr)
+    #sys.exit(1)
+    pass
 
 # Run mikidown
 try:
